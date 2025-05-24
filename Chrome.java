6ca842866ea25596d7c2ed5cda7b4d4454258ed6
maniraj4198 <maniraj4198@gmail.com>
2025-05-24 21:09:19 +0530

    Add files via upload
    Day1 assignment completed

---
+++ Chrome.java
@@ -0,0 +1,20 @@
+package week1Day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		 float chromeversion = 91.01f;
+		 System.out.println(chromeversion);
+		 String developer = "Google";
+		 System.out.println(developer);
+		 boolean beta = false;
+		 System.out.println(beta);
+		 int releaseYear = 2008;
+		 System.out.println(releaseYear);
+		 char shortcutkey = 'C';
+		  System.out.println(shortcutkey);
+		 
+
+	}
+
+}
---
+++ Testleafpractice.java
@@ -0,0 +1,24 @@
+package week1Day1;
+
+public class Testleafpractice {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("Welcome to Testleaf");
+
+		String name = "manikandan";
+
+		System.out.println(name);
+
+		byte age = 27;
+
+		System.out.println(age);
+
+
+		System.out.println("my name is "+name);
+		
+		System.out.println("my age is "+age);
+	}
+
+}
